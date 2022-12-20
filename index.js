093c709665b6d2b43e65b5e3269c48c97d1fa698
Supertiger <saawalkhn@googlemail.com>
2022-12-20 18:34:43 +0000

    Added conversion for categories.json

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const input = require("./input.json");
 
-
 const emojiList = input.map(emoji => ({
     emoji: emoji.emoji,
     short_names: emoji.aliases,
@@ -9,15 +8,19 @@ const emojiList = input.map(emoji => ({
 }))
 
 const shortcodesToEmojis = {};
-
 input.forEach(emoji => {
     emoji.aliases.forEach(shortName => {
         shortcodesToEmojis[shortName] = emoji.emoji;
     });
 });
 
+const categories = new Set();
+input.forEach(emoji => {
+    categories.add(emoji.category);
+});
 
 fs.rmSync("./out", {recursive: true, force: true});
 fs.mkdirSync("./out")
 fs.writeFileSync('./out/emoji-shortcodes.json', JSON.stringify(shortcodesToEmojis, null, 2));
 fs.writeFileSync('./out/emoji.json', JSON.stringify(emojiList, null, 2));
+fs.writeFileSync('./out/categories.json', JSON.stringify(new Array(...categories), null, 2));
