17656ab70ef9cd679657382f85dc347b67eee9f8
Supertiger <saawalkhn@googlemail.com>
2023-02-18 11:36:55 +0000

    Updated sprite

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
-const { createCanvas, loadImage } = require('canvas');
 const fs = require('fs');
-const twemoji = require('twemoji');
 const input = require("./input.json");
 
-// console.log(twemoji)
+// emojis that dont exist or are causing problems
+const blacklist = ["👁️‍🗨️"];
 
-const emojiList = input.map(emoji => ({
-    emoji: emoji.emoji,
-    short_names: emoji.aliases,
-    category: emoji.category,
-}))
+const emojiList = input
+    .filter(emoji => !blacklist.includes(emoji.emoji))
+    .map(emoji => ({
+        emoji: emoji.emoji,
+        short_names: emoji.aliases,
+        category: emoji.category,
+    }))
 
 const unicodeToShortcodes = {};
-input.forEach(emoji => {
-    unicodeToShortcodes[emoji.emoji] = emoji.aliases[0];
+emojiList.forEach(emoji => {
+    unicodeToShortcodes[emoji.emoji] = emoji.short_names[0];
 });
 
 const shortcodeToUnicodes = {};
-input.forEach(emoji => {
-    shortcodeToUnicodes[emoji.aliases[0]] = emoji.emoji;
+emojiList.forEach(emoji => {
+    shortcodeToUnicodes[emoji.short_names[0]] = emoji.emoji;
 });
 
 const categories = new Set();
-input.forEach(emoji => {
+emojiList.forEach(emoji => {
     categories.add(emoji.category);
 });
 
@@ -32,63 +33,3 @@ fs.writeFileSync('./output/shortcodes-to-unicode.json', JSON.stringify(shortcode
 fs.writeFileSync('./output/unicode-to-shortcodes.json', JSON.stringify(unicodeToShortcodes, null, 2));
 fs.writeFileSync('./output/emojis.json', JSON.stringify(emojiList, null, 2));
 fs.writeFileSync('./output/categories.json', JSON.stringify(new Array(...categories), null, 2));
-
-
-
-// create twemoji spritesheet
-
-
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-const U200D = String.fromCharCode(0x200d);
-const UFE0Fg = /\uFE0F/g;
-
-const unicodeToTwemojiUrl = (unicode) => {
-  const codePoint = twemoji.convert.toCodePoint(
-    unicode.indexOf(U200D) < 0 ? unicode.replace(UFE0Fg, "") : unicode
-  );
-
-return `https://nerimity.com/twemojis/${codePoint}.svg`
-
-}
-
-
-async function generateSprites() {
-    const SIZE = 40;
-    const ROW = 40;
-
-    let currentColumn = 0;
-    
-    const canvas = createCanvas(SIZE * ROW, 1680)
-    const ctx = canvas.getContext('2d')
-    
-    let count = 0;    
-    for (let index = 0; index < emojiList.length; index++) {
-        const row = index % ROW;
-        const emoji = emojiList[index];
-        const image = await loadImage(unicodeToTwemojiUrl(emoji.emoji)).catch(() => {
-            console.log(`invalid: ${emoji.emoji} ${unicodeToTwemojiUrl(emoji.emoji)}`)
-        });
-        if (!image) continue;
-        if (row === 0 && index !== 0) {
-            currentColumn++;
-        }
-        ctx.drawImage(image, row * SIZE, currentColumn * SIZE , SIZE, SIZE);
-        count++;
-        console.log(`${count}/${emojiList.length}`);
-    
-    }
-
-    const out = fs.createWriteStream(__dirname + '/test.png')
-    const stream = canvas.createPNGStream({compressionLevel: 0, filters: canvas.PNG_NO_FILTERS})
-    stream.pipe(out)
-    out.on('finish', () =>  console.log('The PNG file was created.'))
- 
-}
-
-
-
-generateSprites();
-
